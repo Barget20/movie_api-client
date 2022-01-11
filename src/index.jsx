10ae1bcd85e50d38712417705d59cd0b161c14f2
Brian A <89669426+Barget20@users.noreply.github.com>
2022-01-11 16:34:00 -0500

    Featured Added
    1) created main-view  & movie-card jsx
    2) imported and exported MainView into index.jsx and index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 
 import './index.scss';
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <div className="movie_api">
-                <div>Good morning </div>
-            </div>
+           <MainView />
         );
     }
 }
