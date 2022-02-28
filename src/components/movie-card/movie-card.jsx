6fe9b75161a42368995df23c281905f983cebd28
Brian A <89669426+Barget20@users.noreply.github.com>
2022-02-28 14:05:38 -0500

    Revert "React bootstrap 3.5"

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,71 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import '../movie-card/movie-card.scss';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
-  constructor() {
-    super();
-  }
- 
-/* // Add movie to FavoriteMovies list */
-onAddFavorite = (movie) => {
-  const username = localStorage.getItem('user');
-  const token = localStorage.getItem('token');
+    render() {
+        const {movie, onMovieClick } = this.props;
 
-  axios
-    .post(
-      `https://movie-api-2022.herokuapp.com/users/${username}/favoritesList/${movie._id}`,
-      {},
-      {
-        headers: { Authorization: `Bearer ${token}`},
-      }
-    )
-    .then((response) => {
-      console.log(response);
-      alert("Movie added");
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
-
-render() {
-  const { movie, onBackClick } = this.props;
-  // const {favorites, setFavorites } = this.props;
-  // const addFavoriteMovie = (movie) => {
-  //   const newFavoriteList = {...favorites, movie};
-  //   setFavorites(newFavoriteList);
-  // };
-
-  return (
-    <Card>
-      <Card.Img varient="top" src={movie.ImagePath} />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Link to={`/movies/${movie._id}`}>
-          <Button varient="link">Open</Button>
-        </Link>
-        <Link to={`/directors/${movie.Director.Name}`}>
-          <Button varient="link">Director</Button>
-        </Link>
-        <Button onClick={() => this.onAddFavorite(movie)}>
-          Add Favorite
-        </Button>
-      </Card.Body>
-    </Card>
-    );
-  }
+        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    }
 }
 
-
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string,
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
+    movie: PropTypes.shape({
+        Title: PropTypes.string
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,130 +1,31 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Form, Row, Col, Button, Container } from "react-bootstrap";
+import React, { useState} from 'react';
 
-import "./registration-view.scss";
+import '../registration-view/registration-view.scss';
 
 export function RegistrationView(props) {
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [values, setValues] = useState({
-    nameErr: "",
-    usernameErr: "",
-    passwordErr: "",
-    emailErr: "",
-  });
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
 
-  const validate = () => {
-    let isReq = true;
-    if (!name) {
-      setNameErr("Name is Required");
-      isReq = false;
-    }
-    if (!username) {
-      setUsernameErr("Username Required");
-      isReq = false;
-    }
-    if (!password) {
-      setPasswordErr("Password Required");
-      isReq = false;
-    } else if (password.length < 6) {
-      setPassword("Password must be 6 characters long");
-    }
-    return isReq;
-    if (!email) {
-      setEmailErr("Email is Required");
-      isReq = false;
-    } else if (email.indexOf("@") === -1) {
-      setEmail("Email is invalid");
-    }
-    return isReq;
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        // sends a request to the server for authentication, 
+        // then call props.onLoggedIn(ushername)
+        props.onLoggedIn(username)
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isReq = validate();
-    if (isReq) {
-      axios
-        .post("https://movie-api-2022.herokuapp.com/users", {
-          Name: name,
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday,
-        })
-        .then((response) => {
-          const data = response.data;
-          console.log(data);
-          alert("Registration successful, please login.");
-          window.open("/", "_self");
-        })
-        .catch((e) => {
-          console.error("error registering the user");
-        });
-    }
-  };
 
-  return (
-    <Form>
-      <Form.Group controlId="formName">
-        <Form.Label>Name:</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        {values.nameErr && <p>{values.nameErr}</p>}
-      </Form.Group>
-
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        {values.usernameErr && <p>{values.usernameErr}</p>}
-      </Form.Group>
-
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {values.passwordErr && <p>{values.passwordErr}</p>}
-      </Form.Group>
-
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {values.emailErr && <p>{values.emailErr}</p>}
-      </Form.Group>
-
-      <Form.Group controlId="updateBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date"
-          placeholder="Enter birthday"
-          onChange={(e) => setBirthday(e.target.value)}
-        />
-      </Form.Group>
-
-      <Button varient="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
-  );
+        return (
+            <form>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>    
+                    <button type="button" onClick={handleSubmit}>Submit</button>
+            </form>
+        );
 }
